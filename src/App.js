5e3ab08353c91792b738d5ece298ea8592feef20
SerePate <serepate@umail.iu.edu>
2017-07-11 17:44:53 -0400

    updated to class version + nasa example added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
-import {Route, Switch, NavLink} from 'react-router-dom'
+import { Switch, Route, NavLink } from 'react-router-dom'
 import Github from './Github'
+import Nasa from './Nasa'
+import Homework from './Homework'
+import './App.css';
 
 class App extends Component {
   render() {
@@ -9,22 +11,29 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <div className="App-headings">
-            <h3>Ain't no party like an </h3>
-            <h1>API PARTY</h1>
+            <h3>Ain't no party like an</h3>
+            <h1>API Party</h1>
           </div>
           <ul className="nav-links">
             <li>
-              <NavLink to="/github">GitHub API</NavLink>
+              <NavLink to='/github'>Github API</NavLink>
+            </li>
+            <li>
+              <NavLink to='/nasa'>NASA API</NavLink>
+            </li>
+            <li>
+              <NavLink to='/homework'>Homework</NavLink>
             </li>
           </ul>
         </div>
         <Switch>
-          <Route path='/github' render={() => <h1>Github!</h1>}/>
-          <Route render={() => <p> To get started, click one  of the links above</p>}/>
+          <Route path='/github' component={Github} />
+          <Route path='/nasa' component={Nasa} />
+          <Route path='/homework' component={Homework} />
+          <Route render={() => <p>To get started, click one of the links above</p>} />
         </Switch>
       </div>
     );
   }
 }
-
 export default App;
